c8abbe00246e9542489626e92d6ac2478b07863c
Mišo <mdzeko@foi.hr>
2017-01-25 23:28:17 +0100

    Dodani osnovni elementi sučelja (tabovi). Omogućeno pokretanje glavne forme pri pokretanju aplikacije.

--- src/hr/view/Main.java
+++ src/hr/view/Main.java
@@ -3,7 +3,19 @@ package hr.view;
 import javax.swing.*;
 /**
  * Created by msaponja on 25.01.17..
+ * OK
  */
 public class Main {
     private JPanel mainPane;
+    private JTabbedPane mainTabPane;
+
+    public static void main(String[] args) {
+        Main mainWindowObject = new Main();
+
+        JFrame mainWindow = new JFrame("TBP - skladiste");
+        mainWindow.setContentPane(mainWindowObject.mainPane);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.pack();
+        mainWindow.setVisible(true);
+    }
 }
